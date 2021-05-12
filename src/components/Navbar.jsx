e7a3f19e9f3486fe6b07448cee5d1504548aa73a
Robert Ruiz <rruiz2298@gmail.com>
2021-05-12 11:42:54 -0400

    Agregando webpack,babel,eslint,devserver

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar__logo">
+                <img src="" alt="" />
+            </div>
+            
+        </div>
+    );
+}
+
+export default Navbar;
